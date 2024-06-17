a3d4f98a7739677c91eb007aab230dba458ea643
Norton Almeida Junior <norton.junior@defensoria.mg.def.br>
2024-06-17 13:32:35 -0300

    adjusting docker ports

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     render json: @users.to_json(
-      include: { group: { except: %i[created_at updated_at] },
-                 car: { except: %i[created_at updated_at user_id] } }, except: [:group_id]
+      include: { group: { except: %i[created_at updated_at] } }, except: [:group_id]
     )
   end
 end
